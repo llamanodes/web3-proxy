b16fbf090e5510a15f5dc02ccbf7d8e187ff9426
Bryan Stitt <bryan@satoshiandkin.com>
2022-04-25 19:46:17 +0000

    subscribe instead of watch

--- examples/subscribe_blocks.rs
+++ examples/subscribe_blocks.rs
@@ -3,12 +3,11 @@ use std::time::Duration;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    // let ws = Ws::connect("ws://10.11.12.16:8545").await?;
-    let ws = Ws::connect("ws://10.11.12.16:8946").await?;
+    let ws = Ws::connect("ws://10.11.12.16:8545").await?;
+    // let ws = Ws::connect("ws://10.11.12.16:8946").await?;
     let provider = Provider::new(ws).interval(Duration::from_secs(1));
-    let mut stream = provider.watch_blocks().await?.take(5);
-    while let Some(block_number) = stream.next().await {
-        let block = provider.get_block(block_number).await?.unwrap();
+    let mut stream = provider.subscribe_blocks().await?.take(5);
+    while let Some(block) = stream.next().await {
         println!(
             "Ts: {:?}, block number: {} -> {:?}",
             block.timestamp,
