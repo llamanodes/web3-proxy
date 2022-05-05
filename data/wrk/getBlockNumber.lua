bc91bd1c6fe9ccc6f84af2fe19805b1ef16131e9
Bryan Stitt <bryan@satoshiandkin.com>
2022-05-05 19:06:03 +0000

    first pass at caching
    i'm confused. i had it over 100k connections on friday, but now even when i go back to those commits, i can't get that high

--- data/wrk/getBlockNumber.lua
+++
@@ -1,3 +0,0 @@
-wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_blockNumber\",\"params\":[],\"id\":420}"
-wrk.headers["Content-Type"] = "application/json"
--- data/wrk/getLatestBlockByNumber.lua
+++
@@ -1,3 +0,0 @@
-wrk.method = "POST"
-wrk.body   = "{\"jsonrpc\":\"2.0\",\"method\":\"eth_getBlockByNumber\",\"params\":[\"latest\", false],\"id\":420}"
-wrk.headers["Content-Type"] = "application/json"
