eb96ab0a6c88e6040756cdf3911002f8ae0a323e
Bryan Stitt <bryan@satoshiandkin.com>
2022-11-16 21:21:27 +0000

    actually save

--- web3_proxy/src/bin/web3_proxy_cli/change_user_tier_by_key.rs
+++ web3_proxy/src/bin/web3_proxy_cli/change_user_tier_by_key.rs
@@ -3,7 +3,8 @@ use argh::FromArgs;
 use entities::{rpc_key, user, user_tier};
 use log::{debug, info};
 use migration::sea_orm::{
-    self, ColumnTrait, DatabaseConnection, EntityTrait, IntoActiveModel, QueryFilter,
+    self, ActiveModelTrait, ColumnTrait, DatabaseConnection, EntityTrait, IntoActiveModel,
+    QueryFilter,
 };
 use uuid::Uuid;
 use web3_proxy::frontend::authorization::RpcSecretKey;
@@ -51,6 +52,8 @@ impl ChangeUserTierByKeyCommand {
             let mut user = user.into_active_model();
 
             user.user_tier_id = sea_orm::Set(user_tier.id);
+
+            user.save(db_conn).await?;
 
             info!("user's tier changed");
         }
