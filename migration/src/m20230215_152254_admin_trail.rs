499610add7b696a939c5d949d0f490d27696bc7c
yenicelik <david.yenicelik@gmail.com>
2023-02-15 16:29:30 +0100

    added admin trail table

---
+++ migration/src/m20230215_152254_admin_trail.rs
@@ -0,0 +1,93 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(AdminTrail::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(AdminTrail::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(
+                        ColumnDef::new(AdminTrail::Caller)
+                            .big_unsigned()
+                            .not_null()
+                            // TODO: Add Foreign Key
+                    )
+                    .foreign_key(
+                        sea_query::ForeignKey::create()
+                            .from(AdminTrail::Table, AdminTrail::Caller)
+                            .to(User::Table, User::Id),
+                    )
+                    .col(
+                        ColumnDef::new(AdminTrail::ImitatingUser)
+                            .big_unsigned()
+                            // Can be null bcs maybe we're just logging in / using endpoints that don't imitate a user
+                            // TODO: Add Foreign Key
+                    )
+                    .foreign_key(
+                        sea_query::ForeignKey::create()
+                            .from(AdminTrail::Table, AdminTrail::ImitatingUser)
+                            .to(User::Table, User::Id),
+                    )
+                    .col(
+                        ColumnDef::new(AdminTrail::Endpoint)
+                            .string()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(AdminTrail::Payload)
+                            .string()
+                            .not_null()
+                    )
+                    .col(
+                        ColumnDef::new(AdminTrail::Timestamp)
+                            .timestamp()
+                            .not_null()
+                            .extra("DEFAULT CURRENT_TIMESTAMP".to_string())
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Replace the sample below with your own migration scripts
+        manager
+            .drop_table(Table::drop().table(AdminTrail::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum AdminTrail {
+    Table,
+    Id,
+    Caller,
+    ImitatingUser,
+    Endpoint,
+    Payload,
+    Timestamp
+}
+
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum User {
+    Table,
+    Id,
+    Address,
+    Description,
+    Email,
+}
