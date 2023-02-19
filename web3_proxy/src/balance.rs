41187cef319a7cba3bf4401aedca757cd855e641
yenicelik <david.yenicelik@gmail.com>
2023-02-19 15:17:46 +0100

    will merge PR from stats-v2

--- web3_proxy/src/balance.rs
+++ web3_proxy/src/balance.rs
@@ -6,5 +6,7 @@ use crate::frontend::errors::FrontendErrorResponse;
 /// Adds an entry to the `spent` table (rpc_requests)
 /// Then subtracts the user balance from the `balance` table
 pub async fn update_balance_and_spend(user_id: u64, db_conn: &DatabaseConnection, amount_delta: u64) -> Result<u64, FrontendErrorResponse> {
+
+    // Add a new entry into the rpc endpoint ...
     todo!()
 }
