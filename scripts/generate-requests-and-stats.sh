e35629dc29e4fa475268ef156812e3164228152e
yenicelik <david.yenicelik@gmail.com>
2023-06-08 23:57:46 +0200

    balance is inside the influx query again

--- scripts/generate-requests-and-stats.sh
+++ scripts/generate-requests-and-stats.sh
@@ -5,4 +5,6 @@
 # https://github.com/INFURA/versus
 # ./ethspam | ./versus --stop-after 100 "http://localhost:8544/" # Pipe into the endpoint ..., add a bearer token and all that
 
-./ethspam http://127.0.0.1:8544/rpc/01H0ZZJDNNEW49FRFS4D9SPR8B | ./versus --concurrency=4 --stop-after 100 http://localhost:8544/rpc/01H0ZZJDNNEW49FRFS4D9SPR8B
+./ethspam http://127.0.0.1:8544/rpc/01H2D5DN4D423VR2KFWBZE46TR | ./versus --concurrency=4 --stop-after 10000 http://localhost:8544/rpc/01H2D5DN4D423VR2KFWBZE46TR
+
+./ethspam http://127.0.0.1:8544/rpc/01H2D5CAP1KF2NKRS30SGATDSD | ./versus --concurrency=4 --stop-after 10000 http://localhost:8544/rpc/01H2D5CAP1KF2NKRS30SGATDSD
