a28974b250c3fb273eafd0915b86dff502432efe
Bryan Stitt <bryan@llamanodes.com>
2023-06-19 09:06:10 -0700

    wip

---
+++ web3_proxy/src/compute_units.rs
@@ -0,0 +1,7 @@
+//! Compute Units based on median request latencies and sizes.
+//! Designed to match Alchemy's system.
+//! I'm sure there will be memes about us copying, but the user experience of consistency makes a lot of sense to me.
+//! TODO? pricing based on latency and bytes and
+//! TODO: rate limit on compute units
+//! TODO: pricing on compute units
+//! TODO: script that queries influx and calculates observed relative costs
