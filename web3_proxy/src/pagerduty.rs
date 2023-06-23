bb0f48dd5cf1f23f2ef07e896dc3224d9a2474f2
Bryan Stitt <bryan@llamanodes.com>
2023-06-23 14:46:34 -0700

    use just one hostname crate

--- web3_proxy/src/pagerduty.rs
+++ web3_proxy/src/pagerduty.rs
@@ -1,5 +1,4 @@
 use crate::config::TopConfig;
-use gethostname::gethostname;
 use pagerduty_rs::eventsv2sync::EventsV2 as PagerdutySyncEventsV2;
 use pagerduty_rs::types::{AlertTrigger, AlertTriggerPayload, Event};
 use serde::Serialize;
@@ -24,7 +23,7 @@ use tracing::{debug, error, warn};
             .and_then(|x| x.app.redirect_public_url.clone());
 
         panic::set_hook(Box::new(move |x| {
-            let hostname = gethostname().into_string().unwrap_or("unknown".to_string());
+            let hostname = hostname.get().into_string().unwrap_or("unknown".to_string());
             let panic_msg = format!("{} {:?}", x, x);
 
             if panic_msg.starts_with("panicked at 'WS Server panic") {
@@ -162,10 +161,13 @@ pub fn pagerduty_alert<T: Serialize>(
     let group = chain_id.map(|x| format!("chain #{}", x));
 
     let source = source.unwrap_or_else(|| {
-        gethostname().into_string().unwrap_or_else(|err| {
-            warn!("unable to handle hostname: {:#?}", err);
-            "unknown".to_string()
-        })
+        hostname::get()
+            .unwrap()
+            .into_string()
+            .unwrap_or_else(|err| {
+                warn!("unable to handle hostname: {:#?}", err);
+                "unknown".to_string()
+            })
     });
 
     let mut s = DefaultHasher::new();
