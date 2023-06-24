91cbce6ce84154d031de1c46629549c458fa3a6e
Bryan Stitt <bryan@llamanodes.com>
2023-06-24 00:48:56 -0700

    start adding stat for CU

--- web3_proxy/src/stats/stat_buffer.rs
+++ web3_proxy/src/stats/stat_buffer.rs
@@ -27,6 +27,7 @@ pub struct BufferedRpcQueryStats {
     pub sum_response_bytes: u64,
     pub sum_response_millis: u64,
     pub sum_credits_used: Decimal,
+    pub sum_cu_used: Decimal,
     /// The user's balance at this point in time. Multiple queries might be modifying it at once.
     pub latest_balance: Arc<RwLock<Balance>>,
 }
