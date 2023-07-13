9b57399894a16d2f872c472fd2ce1d91d5741f0a
yenicelik <david.yenicelik@gmail.com>
2023-07-13 16:05:55 -0400

    fix default timestamp for stripe table

---
+++ migration/src/m20230713_144446_stripe_default_date_created.rs
@@ -0,0 +1,53 @@
+use chrono;
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let now = chrono::offset::Local::now();
+
+        // Then change all columns to "now"
+        let update_to_current_timestamp = Query::update()
+            .table(StripeIncreaseBalanceReceipt::Table)
+            .values([(StripeIncreaseBalanceReceipt::DateCreated, Some(now).into())])
+            .to_owned();
+
+        manager.exec_stmt(update_to_current_timestamp).await?;
+
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(StripeIncreaseBalanceReceipt::Table)
+                    .modify_column(
+                        ColumnDef::new(StripeIncreaseBalanceReceipt::DateCreated)
+                            .timestamp()
+                            .extra("DEFAULT CURRENT_TIMESTAMP".to_string())
+                            .not_null(),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // Do nothing ...
+        Ok(())
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum StripeIncreaseBalanceReceipt {
+    Table,
+    Id,
+    StripePaymentIntendId,
+    Amount,
+    Currency,
+    Status,
+    DepositToUserId,
+    Description,
+    DateCreated,
+}
