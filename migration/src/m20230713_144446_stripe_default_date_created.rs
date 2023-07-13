49d2d2229f9edd97336b3a4832c5362e96057d22
yenicelik <david.yenicelik@gmail.com>
2023-07-13 16:37:11 -0400

    keep chrono for now

--- migration/src/m20230713_144446_stripe_default_date_created.rs
+++ migration/src/m20230713_144446_stripe_default_date_created.rs
@@ -1,3 +1,4 @@
+// TODO: Try to re-export timestamp from within sea-orm
 use chrono;
 use sea_orm_migration::prelude::*;
 
@@ -7,16 +8,6 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let now = chrono::offset::Utc::now();
-
-        // Then change all columns to "now"
-        let update_to_current_timestamp = Query::update()
-            .table(StripeIncreaseBalanceReceipt::Table)
-            .values([(StripeIncreaseBalanceReceipt::DateCreated, Some(now).into())])
-            .to_owned();
-
-        manager.exec_stmt(update_to_current_timestamp).await?;
-
         manager
             .alter_table(
                 Table::alter()
@@ -29,7 +20,17 @@ impl MigrationTrait for Migration {
                     )
                     .to_owned(),
             )
-            .await
+            .await?;
+
+        let now = chrono::offset::Utc::now();
+
+        // Then change all columns to "now"
+        let update_to_current_timestamp = Query::update()
+            .table(StripeIncreaseBalanceReceipt::Table)
+            .values([(StripeIncreaseBalanceReceipt::DateCreated, Some(now).into())])
+            .to_owned();
+
+        manager.exec_stmt(update_to_current_timestamp).await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
