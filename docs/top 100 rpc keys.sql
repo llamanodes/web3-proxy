4f0493f69effaad7b78e4a0bdcde05e186ae7cc5
Bryan Stitt <bryan@llamanodes.com>
2023-08-07 15:35:10 -0700

    improve sql for top keys

--- docs/top 100 rpc keys.sql
+++ docs/top 100 rpc keys.sql
@@ -1,4 +1,4 @@
-SELECT SUM(`sum_credits_used`) as sum, rpc_accounting_v2.rpc_key_id, rpc_key.user_id, user.email
+SELECT SUM(`sum_credits_used`) as sum, rpc_accounting_v2.rpc_key_id, rpc_key.user_id, concat("0x", hex(user.address)) as address, user.email
 FROM `rpc_accounting_v2`
 JOIN rpc_key ON rpc_accounting_v2.rpc_key_id=rpc_key.id
 JOIN user on rpc_key.user_id=user.id
