ff3ff5d72fbeb3dac488652dd96383d5f7e6aeef
Bryan Stitt <bryan@llamanodes.com>
2023-10-20 11:58:50 -0700

    remove stub script

--- web3_proxy_cli/src/bin/curve_api_checks.rs
+++
@@ -1,9 +0,0 @@
-use web3_proxy::prelude::anyhow;
-
-fn main() -> anyhow::Result<()> {
-    // get curve-api and rpc endpoints from cli flags
-    // do simple checks for proxy version, blockNum, a simple balance call
-    // in parallel check all the curve api endpoints that we expect to pass. many require multiple chains that we don't yet run locally
-
-    Ok(())
-}
